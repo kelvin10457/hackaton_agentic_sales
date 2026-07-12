4745a29f8b84dde01be3ab181624c90c65722908
KENNY CHUNG VELASTEGUI <kennychung28@gmail.com>
2026-07-12 04:07:54 +0000

    cambio para visualizar el fronted r3

--- web/components/chat/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ChatWindow } from "@/components/chat/ChatWindow";
-
-export default function ChatPage() {
-  return (
-    <main className="min-h-screen bg-slate-50 flex items-center justify-center p-6">
-      <ChatWindow />
-    </main>
-  );
-}
